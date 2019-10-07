b5e54c4d0069c7bf0936c8e1ed1b6d625ae8ec8a
Md Hasan <hasan.mmd28@gmail.com>
2019-10-07 12:22:18 -0400

    update

--- HelloWorld/src/md/Hello/HelloWorld.java
+++ HelloWorld/src/md/Hello/HelloWorld.java
@@ -3,6 +3,20 @@ package md.Hello;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world!!!");
+		System.out.println("Welcome to Hello world!!! and math!!!");
+		int[] arr= {2,4,2,4,2,6,7,10};
+		int sum=sumArray(arr);
+		System.out.println("Sum of the arry is = "+sum);
 	}
+	
+	public static int sumArray(int[] arr) {
+		int sum=0;
+		for(int i=0;i<arr.length;i++) {
+		 sum+=arr[i];
+		}
+		return sum;
+	}
+	
+	
+	
 }
