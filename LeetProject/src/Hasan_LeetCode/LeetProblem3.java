c760ba12a2bab486de8227747a9d1651072dc535
Md Hasan <hasan.mmd28@gmail.com>
2019-11-15 10:38:13 -0500

    updating

--- LeetProject/src/Hasan_LeetCode/LeetProblem3.java
+++ LeetProject/src/Hasan_LeetCode/LeetProblem3.java
@@ -1,5 +1,8 @@
 package Hasan_LeetCode;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /*
  * Problem3. Given a String, find the length of the longest substring 
  * without repeating characters. Examples: Given "abcabcbb", the answer 
@@ -10,10 +13,33 @@ package Hasan_LeetCode;
  */
 
 public class LeetProblem3 {
-
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String s="abcabcbb";
+		int length=lengthOfLongestSubstring(s);
+		System.out.println("Lenght= "+length);
+		
 
+	}
+	public static int lengthOfLongestSubstring(String s) {
+		int length=0;
+		int n=s.length();
+		for(int i=0;i<n;i++) {
+			for(int j=i+1;j<=n;j++) {
+				if(allUnique(s,i,j)) length=Math.max(length,j-i);
+			}
+		}
+		return length;
+	}
+	
+	public static boolean allUnique(String s, int start, int end) {
+		Set<Character> set= new HashSet<>();
+		for(int i=start;i<end;i++) {
+			Character ch=s.charAt(i);
+			if(set.contains(ch)) return false;
+			set.add(ch);
+		}
+		return true;
 	}
 
 }
