e7891a6a43cfc0189bb7d703891ec11b2065923e
MD HASAN <hasan.mmd28@gmail.com>
2020-03-06 10:48:05 -0500

    P7

--- LeetProject/src/Hasan_LeetCode/Problem7.java
+++ LeetProject/src/Hasan_LeetCode/Problem7.java
@@ -1,10 +1,33 @@
 package Hasan_LeetCode;
 
+
+
+
 public class Problem7 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		int x=-3754440;
+		int rev= reverseInteger(x);
+		System.out.println("Reveres of "+x+" is = "+rev);
+		
+		}
+		
+		public static int reverseInteger(int x){
+			int reverse=0;
+			
+			while(x!=0){
+				int pop= x%10;
+				x/=10;
+				//integer max value= 2147483647
+	            //integer min value= -2147483648
+	           if(reverse>Integer.MAX_VALUE/10 || reverse==Integer.MAX_VALUE/10 && pop>7) return 0;
+	           if(reverse<Integer.MIN_VALUE/10 || reverse==Integer.MIN_VALUE/10 && pop<-8) return 0;
+			   
+	           reverse=reverse*10+pop;
+			}
+			
+			return reverse;
+		}
 
 	}
-
-}
