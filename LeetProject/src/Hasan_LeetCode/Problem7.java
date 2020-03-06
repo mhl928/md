46198673a8355abb426f6ebc3585771ff8396f57
MD HASAN <hasan.mmd28@gmail.com>
2020-03-06 10:54:41 -0500

    P7

--- LeetProject/src/Hasan_LeetCode/Problem7.java
+++ LeetProject/src/Hasan_LeetCode/Problem7.java
@@ -1,6 +1,25 @@
 package Hasan_LeetCode;
 
+/*
+ * 
+ * Given a 32-bit signed integer, reverse digits of an integer.
 
+Example 1:
+
+Input: 123
+Output: 321
+Example 2:
+
+Input: -123
+Output: -321
+Example 3:
+
+Input: 120
+Output: 21
+Assume we are dealing with an environment which could only store integers
+ Note: within the 32-bit signed integer range:  -2^31 to 2^31-1 .For the purpose 
+ of this problem, assume that your function returns 0 when the reversed integer overflows.
+ */
 
 
 public class Problem7 {
