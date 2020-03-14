29921bb55f28882ec67f4b821ac80146cedf807e
Md Hasan <hasan.mmd28@gmail.com>
2020-03-13 21:27:44 -0400

    P44

---
+++ LeetProject/src/Hasan_LeetCode/Problem44.java
@@ -0,0 +1,103 @@
+package Hasan_LeetCode;
+/*
+ *Wildcard Matching:
+ * 
+ * Given an input string (s) and a pattern (p), implement wildcard pattern matching with support for '?' and '*'.
+
+'?' Matches any single character.
+'*' Matches any sequence of characters (including the empty sequence).
+The matching should cover the entire input string (not partial).
+
+Note:
+
+s could be empty and contains only lowercase letters a-z.
+p could be empty and contains only lowercase letters a-z, and characters like ? or *.
+Example 1:
+
+Input:
+s = "aa"
+p = "a"
+Output: false
+Explanation: "a" does not match the entire string "aa".
+Example 2:
+
+Input:
+s = "aa"
+p = "*"
+Output: true
+Explanation: '*' matches any sequence.
+Example 3:
+
+Input:
+s = "cb"
+p = "?a"
+Output: false
+Explanation: '?' matches 'c', but the second letter is 'a', which does not match 'b'.
+Example 4:
+
+Input:
+s = "adceb"
+p = "*a*b"
+Output: true
+Explanation: The first '*' matches the empty sequence, while the second '*' matches the substring "dce".
+Example 5:
+
+Input:
+s = "acdcb"
+p = "a*c?b"
+Output: false
+ */
+public class Problem44 {
+	
+	public static void main(String[] args) {
+		String s="adbacd";
+		String p="a*****a*****?*d";
+		boolean bo=isMatched(s,p);
+		System.out.println("\n Is pattern matched? "+bo);
+	}
+	
+	public static boolean isMatched(String s, String p) {
+		char[] str = s.toCharArray();
+		char[]pattern=p.toCharArray();
+		int patternIndex=0;
+		boolean isFirst=true;
+		
+		// to make a***b*c****d->a*b*c*d
+		for(int i=0;i<pattern.length;i++) {
+			if(pattern[i]=='*') {
+				if(isFirst) {
+					pattern[patternIndex++]=pattern[i];
+					isFirst=false;
+				}
+			}
+			else {
+				pattern[patternIndex++]=pattern[i];
+				isFirst=true;
+			}
+		}
+		
+		// print the new pattern 
+		/*for(int i=0;i<patternIndex;i++) {
+			System.out.print(pattern[i]);
+		}*/
+		boolean T[][]= new boolean[str.length+1][patternIndex+1];
+		
+		if(patternIndex>0 && pattern[0]=='*') {
+			T[0][1]=true;
+		}
+		T[0][0]=true;
+		
+		for(int i=1;i<T.length;i++) {
+			for(int j=1;j<T[0].length;j++) {
+				if(pattern[j-1]=='?'||str[i-1]==pattern[j-1]) {
+					T[i][j]=T[i-1][j-1];
+				}
+				else if(pattern[j-1]=='*') {
+					T[i][j]=T[i-1][j] || T[i][j-1];
+				}
+			}
+		}
+		return T[str.length][patternIndex];
+	}
+
+}
